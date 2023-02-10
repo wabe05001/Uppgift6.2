405bf86b78d7a70904cb99a01978dba716f1ad89
Meysam03 <simon.munter@icloud.com>
2023-02-10 10:52:02 +0100

    Add project files.

---
+++ Uppgift6.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Största(10, 20));
+        int tal1 = int.Parse(Console.ReadLine());
+        int tal2 = int.Parse(Console.ReadLine());  
+    }
+    static int Största(int tal1, int tal2)
+    {
+        if (tal1 > tal2)
+        {
+            return tal1;
+        }
+        else
+        {
+            return tal2;
+        }
+        Console.WriteLine(Största(tal1, tal2));
+
+    }
+}
